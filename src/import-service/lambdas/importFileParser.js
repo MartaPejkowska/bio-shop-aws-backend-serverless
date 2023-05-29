cf99664192132b2fdacf6f82a71b2ab9170bb352
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2023-05-29 17:06:13 +0200

    feat: sending sns in importFileParser

--- src/import-service/lambdas/importFileParser.js
+++ src/import-service/lambdas/importFileParser.js
@@ -1,12 +1,14 @@
 import csv from 'csv-parser'
 import { S3Client, GetObjectCommand, CopyObjectCommand,DeleteObjectCommand} from "@aws-sdk/client-s3";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 const results = [];
-
 
 export const importFileParser= async (event)=>{
 
     const s3= new S3Client();
+    const sqs = new SQSClient({ region: "eu-west-1" });
 
+    const queueUrl="https://sqs.eu-west-1.amazonaws.com/839677929172/catalogItemsQueue"
     const bucket=event.Records[0].s3.bucket.name
     const key=event.Records[0].s3.object.key
 
@@ -17,35 +19,38 @@ export const importFileParser= async (event)=>{
         //Getting Object
     const command = new GetObjectCommand(input);
     const response =  await s3.send(command)
-        //consol every record and after finish move file to parsed folder
-    if(response){
-        response.Body.pipe(csv()).on("data", async(data) => {
-            console.log(data)
+
+
+      const csvFunction= response.Body.pipe(csv()).on("data", async(data) => {
             results.push(data);
-            }).on("end", () => {
-                try{
+            }).on("end", async() => {
+                   try{
                     const copyInput={
                         Bucket: bucket,
                         CopySource: bucket + '/' + key,
                         Key: key.replace('uploaded', 'parsed')
                     }
                     const copyCommand = new CopyObjectCommand(copyInput);
-                    const copyResponse =  s3.send(copyCommand)
-                    console.log('file copied to /parsed folder')
-                    s3.send(new DeleteObjectCommand({Bucket:bucket,Key:key}));
-                    console.log('File deleted from /upload folder')
+                    const copyResponse = await s3.send(copyCommand)
+                    .then(console.log('file copied to /parsed folder'))
+                    .then(s3.send(new DeleteObjectCommand({Bucket:bucket,Key:key})))
+                    .then(console.log('File deleted from /upload folder'))
                 }
                 catch{err=>console.log(err)}
-            return results;
-            });
-             }
-    else{
-        const response ={
-            statusCode:500,
-            message:"Error getting Object"}
-        return response
-            }
+                 });
 
-            }
+          for await (const result of csvFunction) {
+              const params = {
+                QueueUrl: queueUrl,
+                MessageBody: JSON.stringify(result),
+              };
+              console.log('params',params)
 
+            const response = await sqs.send(new SendMessageCommand(params))
+            .then(function(result){
+                console.log('result',result)
+              })
+            .catch(err=>console.log(err))
+           }
 
+          }
